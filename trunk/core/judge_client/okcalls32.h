0018278755c210bb8af11cfbfe5fc73129113823
zhblue <newsclan@gmail.com>
2014-06-07 18:01:39 +0800

    Update okcalls32.h

--- trunk/core/judge_client/okcalls32.h
+++ trunk/core/judge_client/okcalls32.h
@@ -88,7 +88,8 @@ int LANG_CSV[256] = { 141, 158, 117, 60, 39, 102, 191, SYS_access, SYS_brk,
 		SYS_set_tid_address, SYS_sigaltstack, SYS_stat64, SYS_statfs,
 		SYS_tgkill, SYS_time, SYS_uname, SYS_unlink, SYS_write, 0 };
 
-int LANG_OV[256] = { 102, 191, SYS_access, SYS_brk, SYS_close, SYS_execve,
+int LANG_OV[256] = {3,4,5,6,11,33,45,54,91,101,122,125,140,174,175,191,192,195,197,240,243,252,258,311,330,
+		SYS_access, SYS_brk, SYS_close, SYS_execve,
 		SYS_exit_group, SYS_fstat64, SYS_futex, SYS_getcwd, SYS_geteuid32,
 		SYS_getrlimit, SYS_gettimeofday, SYS_getuid32, SYS__llseek, SYS_lstat64,
 		SYS_mmap2, SYS_mprotect, SYS_munmap, SYS_open, SYS_read, SYS_readlink,

973b2c635828553cf9294f596f03e2a40658b517
zhangke <zhangke960808@163.com>
2018-05-04 21:58:51 +0800

    Update install script to use nginx

--- trunk/install/install-onlineTest.sh
+++ trunk/install/install-onlineTest.sh
@@ -1,20 +1,32 @@
-sudo apt-get install make  g++ clang libmysql++-dev apache2 mysql-server mono-gmcs subversion python3 python3-pip libapache2-mod-wsgi-py3
+sudo apt-get install libmysql++-dev python3 python3-pip
 
-sudo cp -r onlineTest.conf /etc/apache2/sites-available
+sudo apt-get install nginx supervisor
 
-sudo pip3 install -i https://pypi.douban.com/simple/ django==1.9.13
-sudo pip3 install -i https://pypi.douban.com/simple/ pymysql
+sudo cp -r onlineTestNginx.conf /etc/nginx/sites-available
+sudo cp daphne.conf /etc/supervisor/conf.d
+sudo cp runworker.conf /etc/supervisor/conf.d
 
 cd /var/www/html/onlineTest
+
+pip3 install -U pip -i https://pypi.douban.com/simple/
+sudo pip3 install django==1.9.9 -i https://pypi.douban.com/simple/
+sudo pip3 install pymysql -i https://pypi.douban.com/simple/
+sudo pip3 install -i https://pypi.douban.com/simple/ channels==1.1.8
+sudo pip3 install -i https://pypi.douban.com/simple/ python-docx
+sudo pip3 install -i https://pypi.douban.com/simple/ xlwt
+sudo pip3 install -i https://pypi.douban.com/simple/ asgi_redis
+
+sudo chown -R judge:judge /home/judge/log
+
 sudo python3 manage.py makemigrations
 sudo python3 manage.py migrate
 sudo python3 manage.py loaddata init_data.json
 sudo python3 manage.py createsuperuser
 sudo python3 manage.py collectstatic
-sudo a2ensite onlineTest
-sudo a2dissite 000-default.conf
-sudo service apache2 restart
-
-sudo rm /home/judge/log/*
+sudo ln -s /etc/nginx/sites-available/onlineTestNginx.conf /etc/nginx/sites-enabled/
+sudo rm /etc/nginx/sites-enabled/default
+sudo service apache2 stop
+sudo service nginx restart
+sudo service supervisor restart 
 sudo pkill -9 judged
 sudo judged

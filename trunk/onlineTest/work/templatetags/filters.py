19d934c87113360cde1b233ae6eb6cbb7ced2b7e
xuejing80 <xuejing_cn@163.com>
2020-04-15 07:59:25 +0000

    将waring模块中的自定义filter复制到work模块中

--- trunk/onlineTest/work/templatetags/filters.py
+++ trunk/onlineTest/work/templatetags/filters.py
@@ -14,5 +14,12 @@ def myFilter(value):
     tempstr = tempstr.replace("&lt;pre&gt;","<pre>")
     tempstr = tempstr.replace("&lt;/pre&gt;","</pre>")
     tempstr = tempstr.replace("</pre>\r\n","</pre>")
+    tempstr = tempstr.replace("&lt;code&gt;","<code>")
+    tempstr = tempstr.replace("&lt;/code&gt;","</code>")
+    tempstr = tempstr.replace("</code>\r\n","</code>")
     tempstr = tempstr.replace("|||"," 或者 ") # 填空题答案过滤器
     return tempstr
+
+@register.filter
+def myDiv(value,total):
+        return str("%.2f"%(round((value * 100 / total), 2)))+'%'
